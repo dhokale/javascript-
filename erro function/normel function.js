60fe1bfb106ff408e2863ff7ef0e1a5ff3027d29
deepak dhokale <dhokale.deepak04@gmail.com>
2022-01-07 18:27:37 +0530

    new file add

---
+++ erro function/normel function.js
@@ -0,0 +1,11 @@
+function normel() {
+    class User{
+        constructor( firstName, email){
+            this.firstName=firstName;
+            this.email=email;
+        }
+    }  
+    let user1=newUser(document.getElementById("fname"), document.getElementById("email"));
+document.createElement("th").textContent=user1.firstName;
+document.createElement("th").textContent=user1.email;
+}
