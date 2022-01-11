81406a1d9ecda4908592aa215ec0bea48db87fcf
deepak dhokale <dhokale.deepak04@gmail.com>
2022-01-11 16:39:45 +0530

    erro function

---
+++ erro function/arro function/normel function.js
@@ -0,0 +1,13 @@
+var normel;
+normel=()=>{
+    class User{
+        constructor( firstName, email){
+            this.firstName=firstName;
+            this.email=email;
+        }
+    }
+var a=document.getElementById("fname").value;
+var b= document.getElementById("email").value;  
+    var user1 = new User(a,b);
+    document.write("welcom!"+user1.firstName+"  email is  "+user1.email)
+} 
