d98888785e228ec78aa5c3fa56f9a984c0fa54df
deepak dhokale <dhokale.deepak04@gmail.com>
2022-01-13 13:08:36 +0530

    javascript file change

--- erro function/normel function/normel function.js
+++ erro function/normel function/normel function.js
@@ -1,12 +1,10 @@
-function normel(){
+function normel()
+{
     class User{
         constructor( firstName, email){
-            this.firstName=firstName;
-            this.email=email;
+            this.firstName= firstName;
+            this.email= email;
         }
     }
-var a=document.getElementById("fname").value;
-var b= document.getElementById("email").value;  
-    var user1 = new User(a,b);
-    document.write("welcom!"+user1.firstName+"  email is  "+user1.email)
-} 
+    document.write(this.firstName+""+this.email);
+}
