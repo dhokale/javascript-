122c1b41631888e00e1e303568596c7dd4222f4f
deepak dhokale <dhokale.deepak04@gmail.com>
2022-01-17 15:56:01 +0530

    @@

--- erro function/normel function/normel function.js
+++ erro function/normel function/normel function.js
@@ -1,10 +1,21 @@
-function normel()
-{
-    class User{
-        constructor( firstName, email){
-            this.firstName= firstName;
+
+class User{
+        constructor( fname, email){
+            this.fname= fname;
             this.email= email;
         }
-    }
-    document.write(this.firstName+""+this.email);
+   newRegister()
+   {
+let tr=document.createElement("tr");
+tr.setAttribute("id","myTr");
+document.getElementById("table").appendChild(tr);
+let td=document.createElement("td");
+let  t =document.createTextNode(this.fname);
+td.appendChild(t);
+document.getElementById("myTr").appendChild(td);
+let td1=document.createElement("td");
+let t1 = document.createTextNode(this.email);
+td1.appendChild(t1);
+document.getElementById("myTr").appendChild(td1);
+   } 
 }
